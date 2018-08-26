14011bcb99c7eaa82421a6bd4faef40f12fb055a
DominikDR <dominikdr@protonmail.com>
2018-08-26 13:37:51 +0200

    Make site responsive (#3)
    * Make header responsive
    
    * Revert "Make header responsive"
    
    This reverts commit f438c02a66c49dfb81dd80d34b20a541e83c01ad.
    
    * Make header responsive- in simple way
    
    * Removed content jumping effects after day selecting, tweaks other issues
    
    * Changed tab in code
    
    * Converted tabs to spaces, changed background filenames

--- background/background.js
+++ background/background.js
@@ -1,11 +1,11 @@
 const backgroundsMapping = {
-	"200": "./assets/thunders/thunders.jpg", //Thunderstorm
-	"300": "./assets/rainy/Drizzle.jpg", //Drizzle
-	"500": "./assets/rainy/Image9-kopia.jpg", //Rain
-	"600": "./assets/snowing/snowfalling.jpg", //Snow
-	"800": "./assets/sunny/Image2.jpg", //clear sky (+sun)
-	"801": "./assets/sunny/Image1.jpg", //few clouds (+sun)
-	"802": "./assets/cloudy/Image11.jpg", //scattered clouds
-	"803": "./assets/cloudy/Image5.jpg", //broken clouds
-	"804": "./assets/cloudy/Image5.jpg"
+	"200": "./assets/thunders/thunders.jpg",
+	"300": "./assets/rainy/Drizzle.jpg",
+	"500": "./assets/rainy/Rainy.jpg",
+	"600": "./assets/snowing/snowfalling.jpg",
+	"800": "./assets/sunny/ClearSky.jpg",
+	"801": "./assets/sunny/FewClouds.jpg",
+	"802": "./assets/cloudy/ScatteredClouds.jpg",
+	"803": "./assets/cloudy/BrokenClouds.jpg",
+	"804": "./assets/cloudy/BrokenClouds.jpg"
 }
